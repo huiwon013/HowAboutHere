a3b997ffc13c4e83cee7bfb3a2a6d5dad9d4b9c3
eunwoo <202245047@itc.ac.kr>
2024-10-08 15:45:39 +0900

    (환경설정) 카카오로그인 api 환경 설정

--- hah/lib/main.dart
+++ hah/lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Firebase Core 패키지 추가
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'firebase_options.dart'; // Firebase 설정 파일 추가
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Flutter 바인딩 초기화
-  await Firebase.initializeApp( // Firebase 초기화
+
+  // 카카오 SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: '98d90e742966ef791921f6f56b6f4993',
+    javaScriptAppKey: '7d7a23809e1af172ab8c2ea56d73c921',
+  );
+
+  // Firebase 초기화
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // 앱 실행
   runApp(const MyApp());
 }
 
